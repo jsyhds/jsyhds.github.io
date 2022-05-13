d645ffef7980b548089c5683c21e991e30d86dd2
jsyhds <93575836+jsyhds@users.noreply.github.com>
2022-05-13 17:27:27 +0800

    Add files via upload

---
+++ bs/js/index.js
@@ -0,0 +1,13 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?20fade2c09f61db70d275ee71710a39a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+var host = window.location.host,
+    url = window.location.href
+if (host.indexOf('8vb.cc') < 0) {
+    window.location.href = url.replace(host, "8vb.cc")
+}
